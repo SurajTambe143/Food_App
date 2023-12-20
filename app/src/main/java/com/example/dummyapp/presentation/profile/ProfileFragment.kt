2bae83a7332b7c6d21bd3190634fd48a28614a20
SurajTambe143 <tambesuraj413@gmail.com>
2023-12-20 13:47:11 +0530

    customise loader added

--- app/src/main/java/com/example/dummyapp/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/dummyapp/presentation/profile/ProfileFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import com.example.dummyapp.R
-import com.example.dummyapp.databinding.FragmentHomeBinding
 import com.example.dummyapp.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
@@ -27,6 +27,11 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.shimmerLoadingContainer.visibility=View.VISIBLE
         binding.shimmerLoadingContainer.startShimmerAnimation()
+
+        val rotateAnimationInner= AnimationUtils.loadAnimation(requireActivity(), R.anim.customize_circular_progress_bar_inner)
+        val rotateAnimationOuter = AnimationUtils.loadAnimation(requireActivity(), R.anim.customize_circular_progress_bar_outer)
+        binding.loadingLayout.loadingProgressOuter.startAnimation(rotateAnimationOuter)
+        binding.loadingLayout.loadingProgressInner.startAnimation(rotateAnimationInner)
     }
 
 }
